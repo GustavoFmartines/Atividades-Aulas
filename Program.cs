6f6c36fd41f3366cf3371408eaa30d2bbdfaa704
Gustavo Figueiredo Matines <gustavo.martines2003@outlook.com>
2025-03-14 20:02:23 -0300

    Foram feitos testes de aula no dia 14/03

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string nome;
+string sobrenome;
+
+// Console.WriteLine("Pressione uyma tecla para continuar...");
+// Console.ReadKey(true);
+// Comandos para leitura de mensagens
+
+Console.Clear();
+
+Console.Write("Nome.......: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Sobrenome..:");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Boa noite, {nome} {sobrenome}!");
